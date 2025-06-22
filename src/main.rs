ac321f7926a6eec2c2235a30b33d23c69f320f32
Ahmed Hagi <ahmedosman@mac.com>
2025-06-22 06:28:41 +0100

    refactor: remove unused variables

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,10 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
     let photo_resister = pins.a0.into_analog_input(&mut adc);
-    
-    let mut brightest_light = 1024;
-    let mut darkest_light = 2;
 
     loop {
         let light_value = photo_resister.analog_read(&mut adc);
         ufmt::uwriteln!(&mut serial, "Light value: {}", light_value).unwrap();
-        
-        if light_value < darkest_light {
-            darkest_light = light_value;
-        }
-        
-        if light_value > brightest_light {
-            brightest_light = light_value;
-        }
 
         let delay = match light_value {
             0..=100 => 500,    
